6a9bc3684a9afe4fdd6085de4b73a19b4af29e92
Javad Vatan <Jvatan@yahoo.com>
2016-07-12 20:06:58 +0430

    add enter

--- DownJoz/app/src/main/java/com/vallco/downjoz/MainActivity.java
+++ DownJoz/app/src/main/java/com/vallco/downjoz/MainActivity.java
@@ -1,14 +1,89 @@
 package com.vallco.downjoz;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.vallco.downjoz.App.AppController;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button search_btn ;
+    private EditText inputSearchText;
+    private String tag_json_obj = "json_obj_req";
+    private ProgressDialog pDialog;
+    String url = "link";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initVariable();
+
+
+    }
+    private void searchEvent(String searchText){
+        // Tag used to cancel the request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                pDialog.hide();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("searchText", "searchText");
+                return params;
+            }
+
+        };
+// Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+    }
+    private void initVariable(){
+        pDialog = new ProgressDialog(this);
+        search_btn = (Button) findViewById(R.id.active_search_btn);
+        inputSearchText = (EditText) findViewById(R.id.inputTextSearch_et);
+
+        search_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.active_search_btn:
+                searchEvent(inputSearchText.getText().toString());
+                break;
+        }
     }
 }
